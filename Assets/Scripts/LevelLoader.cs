754cdfe925255490e154fad1693956a266e93a80
Zachary Culpepper <zculp4579@gmail.com>
2021-01-26 18:50:55 -0600

    Version 0.0.0.2
    Added Skybox, Splash Screen, initial Music, and Player Ship asset.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Start()
+    {
+        Invoke("LoadFirstScene", 5f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
